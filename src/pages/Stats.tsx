96981388d1050522b729dd5b221ebf346973ef4a
Kartik Adur <kadur@plagscan.com>
2020-08-17 09:52:36 +0200

    add charts for data

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,6 +1,8 @@
-import React, { ReactElement, FC } from "react";
+import React, { ReactElement, FC, useEffect, useState } from "react";
 import { Navbar } from "../components/Navbar";
 import { useSelector } from "react-redux";
+import { HorizontalBar } from "react-chartjs-2";
+
 import { rootReducerType } from "../store/reducers";
 import { performanceState } from "../store/reducers/performanceReducer";
 import {
@@ -36,17 +38,45 @@ export const Stats: FC = (): ReactElement => {
   //   (state) => state.performance.api
   // );
 
+  const [statData, setStatData] = useState({});
+
   const stats = useSelector<rootReducerType, performanceState["render"]>(
     (state) => state.performance.render
   );
 
   const classes = useStyles();
 
+  const makeChartData = (data: Array<Array<number>>, labels: Array<string>) => {
+    setStatData({
+      labels: labels,
+      datasets: [
+        {
+          data: data,
+          backgroundColor: ["rgba(75, 192, 192, 0.6)"],
+          borderWidth: 4,
+        },
+      ],
+    });
+  };
+
+  useEffect(() => {
+    const labels = Object.keys(stats).map((k: any) => {
+      return stats[k]["id"];
+    });
+    const data = Object.keys(stats).map((k: any) => {
+      return [stats[k]["startTime"], stats[k]["commitTime"]];
+    });
+    makeChartData(data, labels);
+  }, [stats]);
+
   return (
     <>
       <Navbar title="Stats" />
       <h2>Stats Page</h2>
-      <Paper className={classes.inner}>
+      <div>
+        <HorizontalBar data={statData} legend={false} />
+      </div>
+      {/* <Paper className={classes.inner}>
         {Object.keys(stats).map((k: any, id) => (
           <Card key={id} className={classes.root}>
             <CardContent>
@@ -57,7 +87,7 @@ export const Stats: FC = (): ReactElement => {
             </CardContent>
           </Card>
         ))}
-      </Paper>
+      </Paper> */}
     </>
   );
 };
